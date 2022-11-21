29655bda01448e262b5c60dc3c2f7fc19e725e76
itsmeabhishekn <abhishekn4454@gmail.com>
2022-11-21 15:19:22 +0530

    added menu driven options using switchcase

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,39 @@
-import java.util.*
+import java.util.*;
 public class Employee {
+
+
+
+}
+
+class EmployeeData
+{
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("--------MeNu---------\n1.Add Employee\n2.View All\n3.Search Employee\n4.Delete Employee\5.Exit ");
+
+        int ch = reader.nextInt();
+
+        Employee e1 = new Employee();
+
+        switch (ch)
+        {
+            case 1:
+                e1.add();
+                break;
+            case 2:
+                e1.viewAll();
+                break;
+            case 3:
+                e1.searchItem();
+            case 4:
+                e1.deleteItem();
+            case 5:
+                System.exit(0);
+            default:
+                System.out.println("Enter a valid option");
+        }
+
+    }
 }
